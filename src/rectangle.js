a14b1434d4c07dd1fce4abba7d0681ad470f0d64
DesHorsley <deshorsley@hotmail.com>
2013-01-18 09:45:39 +1100

    Adds rectangle

---
+++ src/rectangle.js
@@ -0,0 +1,32 @@
+annotatr.shapes['rectangle'] = (function (annotatr, $, Raphael) {
+    'use strict';
+
+    function draw(element, $container, paper) {
+        var rectangle = paper.rect(
+            element.data.x, element.data.y,
+            element.data.width, element.data.height);
+
+        rectangle.attr('stroke', '#000000');
+        rectangle.attr('stroke-width', 2);
+
+        var objs = {
+            rectangle: rectangle
+        };
+
+        update(element, objs);
+
+        return objs;
+    }
+
+    function update(element, objs) {
+        objs.rectangle.attr('x', element.data.x);
+        objs.rectangle.attr('y', element.data.y);
+        objs.rectangle.attr('width', element.data.width);
+        objs.rectangle.attr('height', element.data.height);
+    }
+
+    return {
+        draw: draw,
+        update: update
+    };
+}(annotatr, window.jQuery, Raphael));
